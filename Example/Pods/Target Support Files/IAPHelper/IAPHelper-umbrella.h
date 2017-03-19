53b02b2939057cc6bfa43f0a81eb100e4f471352
tthufo <tthufo@gmail.com>
2017-03-19 23:41:51 +0700

    dddd

---
+++ Example/Pods/Target Support Files/IAPHelper/IAPHelper-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "IAPHelper.h"
+#import "IAPShare.h"
+#import "NSString+Base64.h"
+#import "SFHFKeychainUtils.h"
+
+FOUNDATION_EXPORT double IAPHelperVersionNumber;
+FOUNDATION_EXPORT const unsigned char IAPHelperVersionString[];
+
